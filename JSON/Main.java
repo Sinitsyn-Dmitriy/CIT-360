8ae19df016525dc0ea841d34253e3c13f9aa09e5
Dmitriy Sinitsyn <2992274@gmail.com>
2018-11-25 21:56:58 -0700

    Update Main.java

--- JSON/Main.java
+++ JSON/Main.java
@@ -4,8 +4,6 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.io.*;
 
-
-
 /**
  *
  * @author dmitriysinitsyn
@@ -23,10 +21,14 @@ public class Main {
             
             System.out.println("Modified User JSON: \n" + userJson);
             
+            System.out.println("\nLoad JSON:");
+            System.out.println("user_id: " + userJson.get("user_id"));
+            System.out.println("last_name: " + userJson.get("last_name"));
+            System.out.println("first_name: " + userJson.get("first_name"));
+            System.out.println("username: " + userJson.get("username"));
+            System.out.println("class: " + userJson.get("class"));
         } 
         catch (IOException | ParseException e) {
         }
     }
 }
-
-
